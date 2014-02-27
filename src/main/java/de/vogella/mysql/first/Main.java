2d677093c63e3d1364923f0ad3161eca62054bf8
Mohamed <filatan@gmail.com>
2014-02-27 16:45:43 +0100

    base de datos en java

---
+++ src/main/java/de/vogella/mysql/first/Main.java
@@ -0,0 +1,12 @@
+package de.vogella.mysql.first;
+
+import de.vogella.mysql.first.App;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    App dao = new App();
+    dao.readDataBase();
+  }
+
+
+} 
